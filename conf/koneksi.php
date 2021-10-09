72a413d95f9d9331695babdb5589a3bd93b5a9c1
revido <revidoberliano@gmail.com>
2021-10-09 14:35:28 +0700

    update koneksi

--- conf/koneksi.php
+++ conf/koneksi.php
@@ -1,3 +1,3 @@
 <?php 
-$con = mysqli_connect("localhost","root","","rapor_lte") or die (mysqli_error());
+$con = mysqli_connect("10.0.0.96","admin","!Pol123Lop!","rapor_lte") or die (mysqli_error());
 ?>
